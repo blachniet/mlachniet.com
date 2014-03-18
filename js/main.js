433178772c960eeaaf1d8abfa9936ae5a1aa8b3d
Brian Lachniet <blachniet@gmail.com>
2014-03-18 18:19:53 -0400

    Added scrollspy. Added navbar transitions. Fixed slider links.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-$('a[href*=#]:not([href=#])').click(function() {
+$('a[href*=#]:not([href=#]):not([href=#carousel-example-generic])').click(function() {
   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
     var target = $(this.hash);
     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
     if (target.length) {
       $('html,body').animate({
         scrollTop: target.offset().top
-      }, 1000);
+      }, 550);
       return false;
     }
   }
