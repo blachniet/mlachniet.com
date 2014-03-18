f2e24b60215ba2505e688026f974a0f01c52116b
Brian Lachniet <blachniet@gmail.com>
2014-03-18 11:53:14 -0400

    Added livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var path = require('path'),
     gulp = require('gulp'),
+    connect = require('connect'),
     cssmin = require('gulp-cssmin'),
+    http = require('http'),
     less = require('gulp-less'),
-    rename = require('gulp-rename'),
-    watch = require('gulp-watch');
+    livereload = require('gulp-livereload'),
+    rename = require('gulp-rename');
 
 gulp.task('less', function(){
   gulp.src(['./less/bootstrap/bootstrap.less', './less/*.less'])
@@ -11,11 +13,25 @@ gulp.task('less', function(){
     .pipe(gulp.dest('./css'))
     .pipe(cssmin({keepSpecialComments: 1}))
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./css'))
+    .pipe(gulp.dest('./css'));
 });
 
 gulp.task('watch', function(){
-    gulp.watch('./less/**/*.less', function(){ gulp.run('less'); });
+    gulp.watch('./less/**/*.less', ['less']);
+
+    // Live Reload
+    var server = livereload();
+    gulp.watch(['./css/**', './fonts/**', './js/**', './*.html'], function(evt){
+        server.changed(evt.path);
+    });
 });
 
-gulp.task('default', ['less']);
+gulp.task('server', ['watch'], function(){
+    var app = connect()
+      .use(connect.logger('dev'))
+      .use(connect.static(__dirname));
+
+    http.createServer(app).listen(3000);
+});
+
+gulp.task('default', ['server']);
