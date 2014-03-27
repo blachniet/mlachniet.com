0101471a24a637dba43f0a3dfb08b2e5c394a29f
Brian Lachniet <blachniet@gmail.com>
2014-03-26 22:02:51 -0400

    Added brand nav animation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$('a[href*=#]:not([href=#]):not([href=#carousel-example-generic])').click(function() {
+$('#nav-links a').click(function() {
   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
     var target = $(this.hash);
     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -9,6 +9,16 @@ $('a[href*=#]:not([href=#]):not([href=#carousel-example-generic])').click(functi
       return false;
     }
   }
+});
+
+$('a.navbar-brand').click(function(event){
+  event.preventDefault();
+  $('html,body').animate({
+    scrollTop: 0
+  }, 550, "linear", function(){
+    // Remove the active nav
+    $('#nav-links li.active').removeClass('active');
+  });
 });
 
 $('.carousel').carousel();
